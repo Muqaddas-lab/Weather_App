09c5245bcaeb9143fef3fe3ce8d7dd5d92b8c07a
Muqaddas-lab <muqaddasfatima200@gmail.com>
2025-04-24 09:33:53 -0700

    initial commit

---
+++ app/src/main/java/com/muqaddas/weather_app/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.muqaddas.weather_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 3000); // 3 second delay
+    }
+}
